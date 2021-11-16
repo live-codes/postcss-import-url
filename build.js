6c15d54ebe5c969c7b29438cc9a1328da50a709b
Hatem Hosny <hatemhosny@gmail.com>
2021-11-16 16:48:20 +0200

    enable browser use and caching

---
+++ build.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+
+const esbuild = require('esbuild');
+const NodeModulesPolyfills = require('@esbuild-plugins/node-modules-polyfill').default;
+const GlobalsPolyfills = require('@esbuild-plugins/node-globals-polyfill').default;
+
+const nodePolyfills = [
+  NodeModulesPolyfills(),
+  GlobalsPolyfills({
+    process: true,
+    buffer: true,
+    define: {
+      global: 'window',
+      'process.env.NODE_ENV': '"production"',
+    },
+  }),
+];
+
+var file_to_patch = path.resolve(__dirname + '/node_modules/postcss/lib/previous-map.js');
+fs.readFile(file_to_patch, 'utf8', function (err, data) {
+  if (err) return console.log(err);
+  var result = data.replace(/require\('fs'\)/g, '{ existsSync: () => false, readFileSync: "" }');
+  fs.writeFile(file_to_patch, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+esbuild.build({
+  entryPoints: ['index.ts'],
+  outfile: 'dist/index.js',
+  format: 'iife',
+  globalName: 'postcssImportUrl',
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  define: {
+    global: 'window',
+    'process.env.NODE_ENV': '"production"',
+  },
+  plugins: nodePolyfills,
+});
---
+++ index.ts
@@ -0,0 +1,119 @@
+import { list, parse } from 'postcss';
+import isUrl from 'is-url';
+import trim from 'lodash.trim';
+import resolveRelative from 'resolve-relative-url';
+import url from 'url';
+
+const defaults = {
+  disableCache: false,
+  recursive: true,
+  resolveUrls: false,
+  modernBrowser: false,
+  userAgent: null,
+};
+const space = list.space;
+const urlRegexp = /url\(["']?.+?['"]?\)/g;
+
+function postcssImportUrl(options: typeof defaults) {
+  options = {
+    ...defaults,
+    ...options,
+  };
+
+  async function importUrl(tree, _, parentRemoteFile) {
+    parentRemoteFile = parentRemoteFile || tree.source.input.file;
+    const imports = [];
+    tree.walkAtRules('import', function checkAtRule(atRule) {
+      const params = space(atRule.params);
+      let remoteFile = cleanupRemoteFile(params[0]);
+      if (parentRemoteFile) {
+        remoteFile = resolveRelative(remoteFile, parentRemoteFile);
+      }
+      if (!isUrl(remoteFile)) {
+        return;
+      }
+      imports[imports.length] = createPromise(remoteFile, options).then(async (r) => {
+        let newNode = parse(r.body);
+        const mediaQueries = params.slice(1).join(' ');
+        if (mediaQueries) {
+          const mediaNode = atRule({
+            name: 'media',
+            params: mediaQueries,
+            source: atRule.source,
+          });
+          mediaNode.append(newNode);
+          newNode = mediaNode as any;
+        } else {
+          newNode.source = atRule.source;
+        }
+
+        if (options.resolveUrls) {
+          // Convert relative paths to absolute paths
+          newNode = newNode.replaceValues(urlRegexp, { fast: 'url(' }, (url) =>
+            resolveUrls(url, remoteFile),
+          );
+        }
+
+        const tree = await (options.recursive
+          ? importUrl(newNode, null, r.parent)
+          : Promise.resolve(newNode));
+        atRule.replaceWith(tree);
+      });
+    });
+    await Promise.all(imports);
+    return tree;
+  }
+
+  return {
+    postcssPlugin: 'postcss-import-url',
+    Once: importUrl,
+  };
+}
+
+module.exports = postcssImportUrl;
+module.exports.postcss = true;
+
+function cleanupRemoteFile(value) {
+  if (value.substr(0, 3) === 'url') {
+    value = value.substr(3);
+  }
+  value = trim(value, '\'"()');
+  return value;
+}
+
+function resolveUrls(to, from) {
+  return 'url("' + resolveRelative(cleanupRemoteFile(to), from) + '")';
+}
+
+const cache: Record<string, string> = {};
+function createPromise(remoteFile, options) {
+  const reqOptions: url.UrlWithStringQuery & { headers?: any } = urlParse(remoteFile);
+  reqOptions.headers = {};
+  reqOptions.headers['connection'] = 'keep-alive';
+  if (options.modernBrowser) {
+    reqOptions.headers['user-agent'] = 'Mozilla/5.0 AppleWebKit/538.0 Chrome/88.0.0.0 Safari/538';
+  }
+  if (options.userAgent) {
+    reqOptions.headers['user-agent'] = String(options.userAgent);
+  }
+  if (options.disableCache !== true && cache[reqOptions.href]) {
+    return Promise.resolve({
+      body: cache[reqOptions.href],
+      parent: remoteFile,
+    });
+  }
+  return fetch(reqOptions.href)
+    .then((res) => res.text())
+    .then((body) => {
+      cache[reqOptions.href] = body;
+      return {
+        body,
+        parent: remoteFile,
+      };
+    });
+}
+
+function urlParse(remoteFile) {
+  const reqOptions = url.parse(remoteFile);
+  return reqOptions;
+}
